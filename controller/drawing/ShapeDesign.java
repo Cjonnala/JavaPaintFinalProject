814b3de07c6fb115728a2987a54a678cb4cb273d
Cjonnala <cjonnala@depaul.edu>
2023-02-09 14:34:56 -0600

    Implemented Ellipse and Triangle shapes with various primary colors

--- controller/drawing/ShapeDesign.java
+++ controller/drawing/ShapeDesign.java
@@ -1,17 +1,21 @@
 package controller.drawing;
 
-import model.ShapeType;
 import model.interfaces.ShapeFrame;
 
 public class ShapeDesign {
+
+    ShapeFrame shapeFrame;
+
     public ShapeFrame makeShape(Shape shape) {
-        ShapeFrame shapeFrame;
-        if (shape.shapeType== ShapeType.RECTANGLE){
-            shapeFrame = new RectShape(shape);
-        }
-        else{
-            shapeFrame = new RectShape(shape);
+
+        switch (shape.shapeType) {
+            case RECTANGLE -> shapeFrame = new RectShape(shape);
+            case ELLIPSE -> shapeFrame = new EllipseShape(shape);
+            case TRIANGLE -> shapeFrame = new TriangleShape(shape);
         }
         return shapeFrame;
     }
 }
+
+
+
